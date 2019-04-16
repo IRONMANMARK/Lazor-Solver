8a87b9858e83a66d5b58a00803111786358a1f13
IRONMANMARK <33693731+IRONMANMARK@users.noreply.github.com>
2019-04-16 16:03:57 -0400

    Delete hello.py

--- commpiled_cython_for_win10/hello.py
+++
@@ -1,8 +0,0 @@
-import hello
-import time
-
-if __name__ =='__main__':
-    start = time.time()
-    hello.MAIN('bff')
-    end = time.time()
-    print('Total run time is %0.2f Seconds' % (end - start))
