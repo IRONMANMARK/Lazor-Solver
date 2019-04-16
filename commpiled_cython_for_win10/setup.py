484140d78757448df2cc2df30e92f3f5b4b8d43e
IRONMANMARK <33693731+IRONMANMARK@users.noreply.github.com>
2019-04-16 18:38:17 -0400

    Add files via upload

--- commpiled_cython_for_win10/setup.py
+++ commpiled_cython_for_win10/setup.py
@@ -2,8 +2,10 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
+# define the name for the c package and the source python file for the c package convert from
 ext_modules = [Extension("hello", ["project.pyx"])]
 
+# set up detail for the build
 setup(
     name='Hello world app',
     cmdclass={'build_ext': build_ext},
